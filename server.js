38b68d89a93ff1bbb9a27dd49de677011099a985
Sireesha Akurathi <sireesha2622@gmail.com>
2024-04-15 09:35:35 +1000

    update

--- server.js
+++ server.js
@@ -6,20 +6,19 @@ const routes = require('./routes/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Connect to MongoDB
+
 connectToDatabase();
 
-// Routes
+
 app.use('/api', routes);
 
-// Serve static files (assuming they are in a 'public' directory)
+
 app.use(express.static('public'));
 
-// Start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
