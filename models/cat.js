4172e040d1a3f1ecbd4f1d37b31eef8b4a904e7a
Sireesha Akurathi <sireesha2622@gmail.com>
2024-04-15 09:31:06 +1000

    commit

--- models/cat.js
+++ models/cat.js
@@ -1,16 +1,23 @@
 const { MongoClient } = require('mongodb');
 
-async function createCat(catData, uri) {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = 'mongodb+srv://sireesha2622:Siri12345@cluster0.ekefhyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+async function createCat(catData) {
+    let client;
 
     try {
+       
+        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
         await client.connect();
-        const database = client.db('yourDatabaseName');
-        const collection = database.collection('cats');
-        
-        // Insert the cat data into the MongoDB collection
-        const result = await collection.insertOne(catData);
-        console.log('Cat created successfully');
+
+        const database = client.db('test'); 
+        const catsCollection = database.collection('Cat');
+
+        const result = await catsCollection.insertOne(catData);
+        console.log('Cat created successfully:', result.insertedId);
+
         return result;
     } catch (error) {
         console.error('Error creating cat:', error);
@@ -21,3 +28,4 @@ async function createCat(catData, uri) {
 }
 
 module.exports = { createCat };
+
